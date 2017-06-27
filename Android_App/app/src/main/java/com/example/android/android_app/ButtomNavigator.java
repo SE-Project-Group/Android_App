ab1708c2e61fd457682dfa2b7097eb712207e1bb
Wang Tao <thor.wang97@outlook.com>
2017-06-27 17:13:03 +0800

    logIn page

---
+++ Android_App/app/src/main/java/com/example/android/android_app/ButtomNavigator.java
@@ -0,0 +1,17 @@
+package com.example.android.android_app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by thor on 2017/6/27.
+ */
+
+public class ButtomNavigator extends LinearLayout {
+    public ButtomNavigator(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+
+}
--- Android_App/app/src/main/java/com/example/android/android_app/LogInActivity.java
+++ Android_App/app/src/main/java/com/example/android/android_app/LogInActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.android_app;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.R.attr.button;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        Button button = (Button) findViewById(R.id.login_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog progressDialog = new ProgressDialog(LogInActivity.this);
+                progressDialog.setTitle("正在注册");
+                progressDialog.setMessage("请稍后");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                if(logIn() == "success"){
+                    Intent intent = new Intent(LogInActivity.this,HomeActivity.class );
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    private String logIn(){
+        String user_name = ((EditText) findViewById(R.id.user_name)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+        // send log in information to server
+
+        // get result
+
+        // if success, get token and store it
+        return "success";
     }
 }
