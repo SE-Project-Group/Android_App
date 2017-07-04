d2aef38647df4ffd02f0e4ac469d2bba3af01902
naiveee <15757402949@163.com>
2017-07-04 11:23:17 +0800

    following page & message items

---
+++ Android_App/app/src/main/java/com/example/android/android_app/User.java
@@ -0,0 +1,28 @@
+package com.example.android.android_app;
+
+/**
+ * Created by jarvis on 2017/7/4.
+ */
+
+public class User {
+    private String user_name;
+    private String last_feed;
+    private int portrait_id;
+
+    public String getUser_name() {
+        return user_name;
+    }
+    public String getLast_feed() {
+        return last_feed;
+    }
+    public int getPortrait_id() {
+        return portrait_id;
+    }
+
+    public User(String user_name,String last_feed, int portrait_id){
+        this.user_name = user_name;
+        this.last_feed = last_feed;
+        this.portrait_id = portrait_id;
+    }
+
+}
