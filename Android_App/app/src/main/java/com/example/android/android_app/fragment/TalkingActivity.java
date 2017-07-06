6548140f31db2307f4dbc0c9ce47d76d7a8a1b21
naiveee <15757402949@163.com>
2017-07-06 14:54:39 +0800

    following page activity

---
+++ Android_App/app/src/main/java/com/example/android/android_app/fragment/TalkingActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.android_app.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.android_app.R;
+
+public class TalkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talking);
+    }
+}
