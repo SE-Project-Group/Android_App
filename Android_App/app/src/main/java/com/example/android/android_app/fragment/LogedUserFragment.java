fb719bf7ed9564d715b1ad369b88276f390d4131
naiveee <15757402949@163.com>
2017-07-13 15:13:19 +0800

    combine loged user fragment & personal page

--- Android_App/app/src/main/java/com/example/android/android_app/fragment/LogedUserFragment.java
+++ Android_App/app/src/main/java/com/example/android/android_app/fragment/LogedUserFragment.java
@@ -1,7 +1,9 @@
 package com.example.android.android_app.fragment;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,12 +12,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-import com.example.android.android_app.MyAlbumActivity;
-import com.example.android.android_app.PersonalPageActivity;
+import com.example.android.android_app.Activity.FansActivity;
+import com.example.android.android_app.Activity.FollowingActivity;
+import com.example.android.android_app.Activity.HomeActivity;
+import com.example.android.android_app.Activity.MyAlbumActivity;
+import com.example.android.android_app.Activity.PersonalHomeActivity;
+import com.example.android.android_app.Activity.PersonalPageActivity;
+import com.example.android.android_app.Activity.PersonalSettingActivity;
+import com.example.android.android_app.Activity.SettingActivity;
 import com.example.android.android_app.R;
-import com.example.android.android_app.SettingActivity;
 
 
 /**
@@ -62,6 +70,56 @@ public class LogedUserFragment extends Fragment{
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), MyAlbumActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout myFollowing = (LinearLayout) getActivity().findViewById(R.id.my_following_layout);
+        myFollowing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FollowingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout myFollower = (LinearLayout) getActivity().findViewById(R.id.my_follower_layout);
+        myFollower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FansActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton personal_setting = (ImageButton) getActivity().findViewById(R.id.personal_setting);
+        personal_setting.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(),PersonalSettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout all_personal_feeds = (LinearLayout) getActivity().findViewById(R.id.all_personal_feeds_layout);
+        all_personal_feeds.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(),PersonalHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout logout_btn = (LinearLayout)getActivity().findViewById(R.id.logout_btn);
+        logout_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                SharedPreferences.Editor editor = getActivity().getSharedPreferences("logIn_data", Context.MODE_PRIVATE).edit();
+                editor.putBoolean("loged",false);
+                editor.putString("token", "");
+                editor.putInt("user_id",0);
+                editor.apply();
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
                 startActivity(intent);
             }
         });
