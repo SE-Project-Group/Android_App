9f462ec76f0b2e2a4f52bf60d615b1551b080eac
Wang Tao <thor.wang97@outlook.com>
2017-08-02 11:16:39 +0800

    配置MyReceiver

--- Android_App/app/src/main/java/com/example/android/track/Receiver/MyReceiver.java
+++ Android_App/app/src/main/java/com/example/android/track/Receiver/MyReceiver.java
@@ -3,6 +3,12 @@ package com.example.android.track.Receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import cn.jpush.android.api.JPushInterface;
+
+import static com.baidu.location.d.j.S;
 
 /**
  * Created by thor on 2017/7/21.
@@ -11,6 +17,15 @@ import android.content.Intent;
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
 
+        // receive custom message
+        if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+            String external = bundle.getString(JPushInterface.EXTRA_EXTRA);
+            String title = bundle.getString(JPushInterface.EXTRA_TITLE);
+            String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
+
+
+        }
     }
 }
